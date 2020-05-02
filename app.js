5b2b0968d0e0d020b2053bb5398f393a6eaaeb38
Max Kohler <max@koehler-kn.de>
2020-05-02 09:26:58 +0100

    Throw out react

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '/client/build/')));
+app.use(express.static(path.join(__dirname, '/client/public/')));
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'))
+    res.sendFile(path.join(__dirname, '/client/public/index.html'))
 })
 
 module.exports = app;

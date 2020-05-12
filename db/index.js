0c9b08660d52ec6219a644ca2c0a74212d12b31f
Max Kohler <max@koehler-kn.de>
2020-05-12 13:12:34 +0100

    remove db info from log

--- db/index.js
+++ db/index.js
@@ -2,7 +2,6 @@ const { Pool } = require('pg')
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL
 })
-console.log(process.env.DATABASE_URL)
 module.exports = {
     query: (text, params, callback) => {
         return pool.query(text, params, callback)

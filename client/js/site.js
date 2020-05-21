56e02b2b36726642852f11fc2e78368d7956c828
MeliBerney <mberney6@hotmail.ch>
2020-05-21 23:42:15 +0200

    Images Height and Click

--- client/js/site.js
+++ client/js/site.js
@@ -75,9 +75,70 @@ function signOut() {
     });
 }
 
+function handleImageClick(e){
+    const container = document.querySelectorAll(".work-images .section-content")
+    container.forEach((c) => {
+        const elements = c.getElementsByTagName("img")
+        var arr = Array.from(elements)
+        const newarr= arr.slice(1);
+
+        const NextImg = e.target.nextElementSibling
+        if (NextImg !== null) {
+        if (NextImg.tagName  === "IMG") {
+            if (NextImg.style.display === "none"){
+                NextImg.style.display = "inline"
+        } 
+    } 
+        } else{
+            if (typeof newarr !== 'undefined' && newarr.length > 0) {
+                for (var i = 0; i < newarr.length; i++) {
+                    (newarr[i]).style.display = "none";
+
+                }
+            }
+        }
+    })
+}
+
+function ImageHeight(){
+    const images = document.querySelectorAll('img')
+    images.forEach(img => {
+        Array.prototype.random = function (length) {
+        return this[Math.floor((Math.random()*length))];
+    }
+    const h = ["3", "4", "6"]
+    const RandomHeight = h.random(h.length) 
+
+        const FinalHeight = RandomHeight + "em";
+        img.style.height = FinalHeight 
+    })
+}
+
+ImageHeight();
+
+function ImageHandle() {
+    const container = document.querySelectorAll(".work-images .section-content")
+    container.forEach((c) => {
+        const elements = c.getElementsByTagName("img")
+        var arr = Array.from(elements)
+        const newarr= arr.slice(1);
+        if (typeof newarr !== 'undefined' && newarr.length > 0) {
+            for (var i = 0; i < newarr.length; i++) {
+                (newarr[i]).style.display = "none";
+            }
+        }
+    })
+    const images = document.querySelectorAll('img')
+    images.forEach(img => {
+        img.addEventListener('click', handleImageClick)
+        img.setAttribute('src', img.getAttribute('data-large'))
+    })
+}
+
+ImageHandle();
 
 function shuffle() {
-    var container = document.querySelector(".works");
+    var container = document.querySelector(".work");
     var elementsArray = Array.prototype.slice.call(container.querySelectorAll('.work'));
     elementsArray.forEach(function (element) {
         container.removeChild(element);
@@ -213,24 +274,6 @@ function CommentList(comments) {
     } else if (fragment.childElementCount === 1) {
         return fragment.children[0];
     }
-}
-
-function handleImageClick(e) {
-    const lb = document.querySelector('.lightbox')
-    const lbImage = lb.querySelector('img')
-    lb.classList.add('active')
-    lbImage.setAttribute('src', e.target.getAttribute('data-large'))
-}
-
-function initLightbox() {
-    const lb = document.querySelector('.lightbox')
-    const images = document.querySelectorAll('img')
-    images.forEach(img => {
-        img.addEventListener('click', handleImageClick)
-    })
-    lb.addEventListener('click', () => {
-        lb.classList.remove('active')
-    })
 }
 
 function renderComments(comments) {

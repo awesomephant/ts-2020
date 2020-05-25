9434296d7bf020322e1b9b0ebbc315c82631a6f0
Max Kohler <max@koehler-kn.de>
2020-05-25 11:55:03 +0100

    Add cursor text

--- client/.eleventy.js
+++ client/.eleventy.js
@@ -27,7 +27,7 @@ module.exports = function (eleventyConfig) {
             return (
                 `<figure style='height: ${height}em' data-original='${url}' data-large="${awsUrl + large}" class='work-figure'>
                     <img style='height: ${height}em' loading="lazy" src="${awsUrl + large}"/>
-                    <button class='figure-expand'>
+                    <button data-cursorText="Expand" class='figure-expand'>
                     <svg style="width:24px;height:24px" viewBox="0 0 24 24">
                     <path d="M5,5H10V7H7V10H5V5M14,5H19V10H17V7H14V5M17,14H19V19H14V17H17V14M10,17V19H5V14H7V17H10Z" />
                  </svg>
